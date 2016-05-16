c94256421c6eb14096cf6f808ac247154933539b
Jim de Beer <jim@vigour.io>
2016-05-16 22:31:19 +0200

    added hub example

---
+++ hub/index.js
@@ -0,0 +1,62 @@
+const Hub = require('vigour-hub')
+const render = require('brisky/render')
+const state = global.state = Hub({ connect: 'ws://localhost:3031' })
+
+state.connected.on(function () {
+  // want to be able to subscribe on this -- important!
+  // add it in client (?)
+  console.log('yo connected!', this.val)
+})
+
+const app = {
+  inputfield: {
+    node: 'input',
+    class: 'basic-item',
+    props: {
+      value: { $: 'field' }
+    },
+    on: {
+      keyup (e, stamp) {
+        console.log(stamp)
+        e.state.set({ field: e.target.value })
+      }
+    }
+  },
+  addMovie: {
+    class: 'basic-item',
+    text: 'add movie',
+    on: {
+      click (e) {
+        var cnt = Date.now()
+        state.set({
+          movies: {
+            [cnt]: {
+              title: 'movie ' + cnt
+            }
+          }
+        })
+      }
+    }
+  },
+  movies: {
+    class: 'holder',
+    title: { text: 'movies' },
+    $: 'movies.$any',
+    Child: {
+      class: 'complex-item',
+      title: { text: { $: 'title' } },
+      removebtn: {
+        class: 'basic-item',
+        text: 'remove',
+        $: true,
+        on: {
+          click (e) {
+            e.state.remove()
+          }
+        }
+      }
+    }
+  }
+}
+
+document.body.appendChild(render(app, state))

2f02de6f7e0c36658d5ec854a88642aaab1b7947
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2016-10-13 15:04:05 +0200

    Started working on showing time beside message

--- chat/index.js
+++ chat/index.js
@@ -2,47 +2,70 @@
 require('./style.css')
 const render = require('brisky/render')
 const s = require('vigour-state/s')
-const state = global.state = s({})
+const state = global.state = s({
+  messageMenus: [ 'Rooms', 'Messages' ]
+})
 
-const channelItem = {
-  class: 'list-item',
-  tag: 'li',
-  link: {
-    class: 'list-link',
-    tag: 'a',
-    text: 'fo shizzle'
+const listMenu = {
+  class: 'menu-wrapper',
+  header: {
+    $: true,
+    class: 'header',
+    title: {
+      class: 'title',
+      tag: 'h2',
+      text: { $: true }
+    }
+  },
+  list: {
+    class: 'menu-list',
+    tag: 'ul',
+    item: {
+      class: 'list-item',
+      tag: 'li',
+      link: {
+        class: 'list-link',
+        tag: 'a',
+        text: 'fo shizzle my nizzle'
+      }
+    }
   }
 }
 
-const channels = {
-  class: 'channel-window',
+const rooms = {
+  class: 'room-window',
   title: {
-    class: 'title',
+    class: 'main-title',
     tag: 'h1',
     text: 'Brisky Chat ™'
   },
-  available: {
-    class: 'channels-menu',
-    header: {
-      class: 'header',
-      title: {
-        class: 'title',
-        tag: 'h2',
-        text: 'Channels:'
-      }
-    },
-    list: {
-      class: 'channels-list',
-      tag: 'ul',
-      channelItem
-    }
-  }
+  username: {
+    class: 'username',
+    tag: 'a',
+    text: 'John Doe'
+  },
+  $: '$root.messageMenus.$any',
+  child: listMenu
 }
 
 const messageItem = {
   class: 'message',
   tag: 'li',
-  text: { $: 'text' }
+  text: '',
+  time: {
+    tag: 'span',
+    text: {
+      $: 'time',
+      $transform: data => {
+        console.log('data: %O', data)
+        return ''
+      }
+    }
+  },
+  message: {
+    tag: 'span',
+    text: { $: 'text' }
+  }
 }
 
 const chat = {
@@ -63,7 +86,7 @@ const chat = {
         enter: (e, stamp) => {
           e.state.set({
             messages: {
-              [Date.now()]: { text: e.target.value }
+              [Date.now()]: { time: Date.now(), text: e.target.value }
             }
           }, stamp)
           e.target.value = ''
@@ -77,7 +100,7 @@ const chatApp = {
   class: 'chat-application',
   main: {
     class: 'main-window',
-    channels,
+    rooms,
     chat
   }
 }
@@ -87,3 +110,10 @@ const app = {
 }
 
 document.body.appendChild(render(app, state))
+
+// let object = { messages: {} }
+// let iteration
+// for (iteration = 0; iteration < 3; iteration++) {
+//   object.messages[iteration] = { text: `message` }
+// }
+// state.set(object)
